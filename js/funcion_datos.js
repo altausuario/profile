e50676d461bb61de74b8d4c4d380ce4b3bc2ec96
Alavaro Tapias <tapiastorrijosalvaro@gmail.com>
2023-08-14 14:19:00 -0500

    Update

--- js/funcion_datos.js
+++ js/funcion_datos.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-    alert()
     $.getJSON('json/datos.json', function (data) {
         var dp = data.DATOS_PERSONALES;
-        $('#nombre').text(dp.cel);
+        var full_name = data.FIRST_NAME + ' ' + data.LAST_NAME
+        $('#nombre').text(full_name);
+        $('#sobre_mi').text(data.ABOUT_ME);
     }).fail(function (jqxhr, textStatus, error) {
         console.error('Error cargando el archivo JSON:', textStatus, error);
     });
